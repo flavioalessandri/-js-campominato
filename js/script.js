e35f4379219892e283a269493207ffc9f6407e23
Flavio <flavio.alessandri@gmail.com>
2020-06-30 17:43:55 +0200

    JS - CPU random array

--- js/script.js
+++ js/script.js
@@ -7,54 +7,14 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 
-var cpuList = [1,2,9,7,5,5,8];
-
-
 //Generate a 16 random numbers array . Value from 0 to 100;
-// var min=0;
-// var max = 100;
-// var arrayDim=6;
-var i =0;
-//
-// for (i=0; i< arrayDim; i++){
-//   var randomNum= Math.floor(Math.random()*(100 - 0 + 1)+1);
-//   cpuList.push(randomNum);
-//   cpuList.sort();
-// }
-// console.log(cpuList);
+var cpuList = myRandomArray(8,0,100);
+console.log(cpuList);
 
-var randomNum= Math.floor(Math.random()*(99 - 0 + 1)+1);
-var count=0;
-
-while(i<cpuList.length){
-  var cpuListSorted = cpuList.sort();
-  if(cpuListSorted[i] != cpuListSorted[i+1]){
-    console.log("fine" , cpuListSorted);
-    count = i;
-  } else{
-    cpuListSorted.splice(i);
-    console.log("numero random",randomNum);
-    console.log("Sbagliato");
-
-  }i++;
+function myRandomArray(arrayLength,min,max){
+  var list=[];
+  while(list.length < arrayLength){
+      var randomNum = Math.floor(Math.random() * (max - min + 1)+min);
+      if(list.indexOf(randomNum) === -1) list.push(randomNum);
+  } return list;
 }
-
-console.log(cpuListSorted);
-
-
-
-
-
-
-
-
-
-
-
-// function arrayRandomElementValue(arraylength , min , max){
-// var random = Math.floor(Math.random()*(max - min + 1)+1);
-
-//
-// }
-
-// function arrayDifferentElementValue(array){}
